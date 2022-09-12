9b97bac705cbcb162b2094cd2b4909c4db8bac11
parvathy <parvathy0722@gmail.com>
2022-09-12 22:50:08 +0530

    added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+class ProductsView(APIView):
+    def get(self,request,*args,**kwargs):
+        return Response({'msg':'inside product get'})
+
+class morning(APIView):
+    def get(self,request,*args,**kwargs):
+
+        return Response({'msg':'good morning'})
